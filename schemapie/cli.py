b43556474d2a657910ecfb30b7fbd098fada8eda
ethancollyer <ethan.l.collyer@gmail.com>
2026-04-29 23:31:18 -0400

    added verbosity to allow for multiple examples in output schema

--- schemapie/cli.py
+++ schemapie/cli.py
@@ -5,8 +5,9 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(prog="schemapie")
     parser.add_argument("url", type=str, help="Complete URL of the desired API route.")
-    parser.add_argument("--file", type=str, help="File name or path. Used to create reusable schemas, update existing schemas, or just to save a schema.")
+    parser.add_argument("--file", type=str, help="File name or path. Use to create reusable schemas, update existing schemas, or just to save a schema.")
     parser.add_argument("--version", action="version", version=f"schemapie {version("schemapie")}", help="Installed version of schemapie.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose Flag. Use to generate multiple examples in the output schema instead of one.")
     args = parser.parse_args()
 
-    execute(url=args.url, file=args.file)
+    execute(url=args.url, file=args.file, verbose=args.verbose)
--- schemapie/utils/api.py
+++ schemapie/utils/api.py
@@ -14,23 +14,27 @@ TYPE_MAP = {
 def get_type(object: any):
     return TYPE_MAP.get(type(object).__name__)
 
-def flatten(data: dict | list, path: str = "", meta: dict = {}):
+def flatten(data: dict | list, path: str = "", meta: dict = None, verbose: bool = False):
     """Flattens each object in the API response into a dictionary object containing {path: details}."""
+    if meta is None:
+        meta = {}
+
     if isinstance(data, dict):
         for k, v in data.items():
-            meta[f"{path}{k}"] = {"path": f"{path}{k}" if path else k, "type": get_type(v)}
+            meta.setdefault(f"{path}{k}", {"path": f"{path}{k}" if path else k, "type": get_type(v)})
             if isinstance(v, (dict, list)):
-                flatten(data=v, path=f"{path}{k}.", meta=meta)
+                flatten(data=v, path=f"{path}{k}.", meta=meta, verbose=verbose)
             else:
-                meta[f"{path}{k}"].update({"example": v})
+                meta[f"{path}{k}"].setdefault("examples", set()).add(v) if verbose else meta[f"{path}{k}"].setdefault("examples", {v})
 
     elif isinstance(data, list):
         for v in data:
             if isinstance(v, (dict, list)):
                 meta[f"{path[:-1]}"].update({"type": f"{get_type(data)}[{get_type(v)}]"})
             else: 
-                meta[f"{path[:-1]}"].update({"type": f"{get_type(data)}[{get_type(v)}]", "example": [v]})
-            flatten(data=v, path=path, meta=meta)
+                meta[f"{path[:-1]}"].update({"type": f"{get_type(data)}[{get_type(v)}]"})
+                meta[f"{path[:-1]}"].setdefault("examples", set()).add(v) if verbose else meta[f"{path[:-1]}"].setdefault("examples", {v})
+            flatten(data=v, path=path, meta=meta, verbose=verbose)
 
     return meta
 
@@ -50,18 +54,20 @@ def build(schema: dict, path: list, value: dict):
             current[p] = {}
 
         if i == len(path) - 1:
+            if value.get("examples"):
+                value["examples"] = list(value["examples"])
             current[p].update(value)
         else:
             current[p].setdefault("children", {})
             current = current[p]["children"]
 
-def execute(url: str, file: str = None):
+def execute(url: str, file: str = None, verbose: bool = False):
     print(f"Fetching data from {url}...")
 
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        meta = flatten(data)
+        meta = flatten(data=data, verbose=verbose)
         schema = merge(meta=meta, schema=read(file=file) if file else {})
         write(schema=schema, file=file) if file else display(schema=schema)
     else:
